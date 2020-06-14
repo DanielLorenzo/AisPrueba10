7a108f5d2c6a97fce86678f3b45af3a7f1dbd084
DanielLorenzo <61579849+DanielLorenzo@users.noreply.github.com>
2020-06-14 03:37:58 +0200

    Add files via upload

---
+++ src/test/java/es/codeurjc/ais/tictactoe/Prueba.java
@@ -0,0 +1,41 @@
+package es.codeurjc.ais.tictactoe;
+
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class Prueba {
+	protected WebDriver driver;
+	 @BeforeAll
+	 public static void setupClass() {
+		 WebDriverManager.chromedriver().setup();
+	 }
+	 @BeforeEach
+	 public void setupTest() {
+		 driver = new ChromeDriver();
+	 }
+	 @AfterEach
+	 public void teardown() {
+		 if (driver != null) {
+		 driver.quit();
+		 }
+	 }
+	 @Test
+	 public void test() {
+		 driver.get("http://www.google.es");
+	 }
+
+}
